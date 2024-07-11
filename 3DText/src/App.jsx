05194990a16dc0006d28630f3cc5fdbac1cd09a3
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-11 15:35:28 +0530

    Starting Buliding 3D Text HeroTag for Home Page

--- 3DText/src/App.jsx
+++ 3DText/src/App.jsx
@@ -1,11 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+      <color attach="background" args={["#171720"]} />
+      <fog attach={"fog"} args={["#171720", 10, 30]} />
       <Experience />
+      <EffectComposer>
+        <Bloom mipmapBlur intensity={1} />
+      </EffectComposer>
     </Canvas>
   );
 }
--- 3DText/src/components/Experience.jsx
+++ 3DText/src/components/Experience.jsx
@@ -1,13 +1,89 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  CameraControls,
+  Environment,
+  Float,
+  MeshReflectorMaterial,
+  OrbitControls,
+  RenderTexture,
+  Text,
+} from "@react-three/drei";
+import { HomeModel } from "./HomeModel";
+import { degToRad } from "three/src/math/MathUtils.js";
+import { useEffect, useRef } from "react";
+import { Color } from "three";
+
+const bloomColor = new Color("#fff");
+bloomColor.multiplyScalar(1.5);
 
 export const Experience = () => {
+  const controls = useRef();
+
+  const intro = () => {
+    controls.current.dolly(-22);
+    controls.current.smoothTime = 1.6;
+    controls.current.dolly(22, true);
+  };
+
+  useEffect(() => {
+    intro();
+  });
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <CameraControls ref={controls} />
+      <Text
+        font={"fonts/Poppins-Black.ttf"}
+        position-x={-1.3}
+        position-y={-0.5}
+        position-z={1}
+        lineHeight={0.8}
+        textAlign="center"
+        rotation-y={degToRad(30)}
+        anchorY={"bottom"}
+      >
+        WELCOME TO{"\n"}ECOMSPHERE
+        <meshBasicMaterial color={bloomColor} toneMapped={false}>
+          <RenderTexture attach={"map"}>
+            <color attach={"background"} args={["#fff"]} />
+            {/* <Environment preset="sunset" /> */}
+            <ambientLight />
+            <Float floatIntensity={4} rotationIntensity={5}>
+              <HomeModel
+                scale={2}
+                rotation-y={-degToRad(25)}
+                rotation-x={degToRad(40)}
+                position-y={-0.5}
+              />
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
+      </Text>
+      <group
+        rotation-y={degToRad(-25)}
+        position-x={4.5}
+        position-z={-1}
+        position-y={-0.5}
+      >
+        <HomeModel scale={1.2} />
+      </group>
+      <mesh position-y={-0.48} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[100, 100]} />
+        <MeshReflectorMaterial
+          blur={[100, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={10}
+          roughness={1}
+          depthScale={1}
+          opacity={0.5}
+          transparent
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#333"
+          metalness={0.5}
+        />
       </mesh>
+      <Environment preset="sunset" />
+      <ambientLight />
     </>
   );
 };
