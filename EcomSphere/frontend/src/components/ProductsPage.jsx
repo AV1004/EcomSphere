95aa73111c067c9f69ec73a07f6194f628fd8a57
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-14 16:24:19 +0530

    Starting the Products Page

--- EcomSphere/frontend/src/components/ProductsPage.jsx
+++ EcomSphere/frontend/src/components/ProductsPage.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Scroll } from "@react-three/drei";
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Button,
+} from "@material-tailwind/react";
 
 export default function ProductsPage() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <Canvas shadows camera={{ position: [0, 2.5, 8.5], fov: 42 }}>
-      <OrbitControls />
-      <color attach={"background"} args={["#171720"]} />
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshBasicMaterial color={"red"} transparent opacity={0.5} />
-      </mesh>
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [0, 2.5, 8.5], fov: 42 }}>
+        <OrbitControls />
+        <color attach={"background"} args={["#171720"]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshBasicMaterial color={"red"} transparent opacity={0.5} />
+        </mesh>
+      </Canvas>
+      <div className="fixed inset-0 pointer-events-none">
+        <section className={`flex w-[10%]  m-20  flex-col    duration-500`}>
+          <Menu
+            animate={{
+              mount: { y: 0 },
+              unmount: { y: 25 },
+            }}
+          >
+            <MenuHandler>
+              <Button> All Products</Button>
+            </MenuHandler>
+            <MenuList>
+              <MenuItem>Menu Item 1</MenuItem>
+              <MenuItem>Menu Item 2</MenuItem>
+              <MenuItem>Menu Item 3</MenuItem>
+            </MenuList>
+          </Menu>
+        </section>
+      </div>
+    </>
   );
 }
