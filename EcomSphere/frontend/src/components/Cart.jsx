e8a69c8d83365f2cbb8d43a87370ad8c5eeac498
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-22 13:59:00 +0530

    Adding Fallback for Empty Cart

--- EcomSphere/frontend/src/components/Cart.jsx
+++ EcomSphere/frontend/src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import {
   Card,
   Typography,
@@ -17,6 +17,9 @@ import {
   getCartItems,
   reomveProdFromCart,
 } from "../https/shop";
+
+import emptyCart from "/images/emptyCart.png";
+import { Link } from "react-router-dom";
 
 const TABLE_HEAD = ["Products", "Quantity", "Subtotal"];
 
@@ -102,196 +105,217 @@ export default function Cart() {
         className={`flex w-full h-full flex-col lg:pt-12 pt-16 duration-500 overflow-auto scrollbar-thin scrollbar-webkit`}
       >
         {TABLE_ROWS.length === 0 && showFallBackText === true ? (
-          <div className="pointer-events-auto  flex justify-center items-center lg:justify-start lg:items-start">
-            <Typography color="white" className="lg:text-3xl text-2xl">
-              Your cart is empty!
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1.3 }}
+            className="pointer-events-auto  flex flex-col gap-3 justify-center items-center h-full"
+          >
+            <img
+              src={emptyCart}
+              alt={"emptyCart"}
+              className="lg:h-96 lg:w-96 h-80 w-80"
+            />
+            <Typography color="white" className="lg:tex-4xl text-xl">
+              EMPTY CART
             </Typography>
-          </div>
+            <Link to={"/products"}>
+              <Button color="teal">Explore Products</Button>
+            </Link>
+          </motion.div>
         ) : (
           <div className="pointer-events-auto  flex justify-center items-center lg:justify-start lg:items-start">
-            <Card className="h-full w-full overflow-hidden bg-[#171720] lg:mx-20 ">
-              <CardBody className="px-0 lg:h-[30.9rem] h-[42rem] overflow-auto scrollbar-thin scrollbar-webkit">
-                <table className="w-full min-w-max lg:table-auto table-fixed text-left">
-                  <motion.thead
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 1.3 }}
-                  >
-                    <tr>
-                      {TABLE_HEAD.map((head) => (
-                        <th
-                          key={head}
-                          className="border-y border-blue-gray-100 w-1 bg-blue-gray-700 p-4"
-                        >
-                          <Typography
-                            // variant={isMoblie ? "small" : "h5"}
-                            color="white"
-                            className="leading-none opacity-70 lg:text-lg text-sm  font-semibold"
+            {TABLE_ROWS.length !== 0 && showFallBackText !== true ? (
+              <Card className="h-full w-full overflow-hidden bg-[#171720] lg:mx-20 ">
+                <CardBody className="px-0 lg:h-[30.9rem] h-[42rem] overflow-auto scrollbar-thin scrollbar-webkit">
+                  <table className="w-full min-w-max lg:table-auto table-fixed text-left">
+                    <motion.thead
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0 }}
+                      transition={{ duration: 1.3 }}
+                    >
+                      <tr>
+                        {TABLE_HEAD.map((head) => (
+                          <th
+                            key={head}
+                            className="border-y border-blue-gray-100 w-1 bg-blue-gray-700 p-4"
                           >
-                            {head}
-                          </Typography>
-                        </th>
-                      ))}
-                    </tr>
-                  </motion.thead>
-                  <tbody className="border-0">
-                    {TABLE_ROWS.map(
-                      (
-                        // { img, name, price, total, quantity, category },
-                        item,
-                        index
-                      ) => {
-                        return (
-                          <motion.tr
-                            key={index}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 1, delay: 1 + index * 0.2 }}
-                          >
-                            <td className="p-4">
-                              <div className="flex lg:flex-row flex-col items-center gap-3">
-                                <img
-                                  src={item.productId.imageUrl.url}
-                                  alt={item.productId.name}
-                                  className="lg:h-24 lg:w-24 h-16 w-16"
-                                />
-                                <div className="flex flex-col gap-2 justify-start">
-                                  <Typography
-                                    variant="h6"
-                                    color="white"
-                                    className="font-bold flex lg:flex-row flex-col gap-2"
-                                  >
-                                    {item.productId.name}
+                            <Typography
+                              // variant={isMoblie ? "small" : "h5"}
+                              color="white"
+                              className="leading-none opacity-70 lg:text-lg text-sm  font-semibold"
+                            >
+                              {head}
+                            </Typography>
+                          </th>
+                        ))}
+                      </tr>
+                    </motion.thead>
+                    <tbody className="border-0">
+                      {TABLE_ROWS.map(
+                        (
+                          // { img, name, price, total, quantity, category },
+                          item,
+                          index
+                        ) => {
+                          return (
+                            <motion.tr
+                              key={index}
+                              initial={{ opacity: 0 }}
+                              animate={{ opacity: 1, y: 0 }}
+                              exit={{ opacity: 0 }}
+                              transition={{
+                                duration: 1,
+                                delay: 1 + index * 0.2,
+                              }}
+                            >
+                              <td className="p-4">
+                                <div className="flex lg:flex-row flex-col items-center gap-3">
+                                  <img
+                                    src={item.productId.imageUrl.url}
+                                    alt={item.productId.name}
+                                    className="lg:h-24 lg:w-24 h-16 w-16"
+                                  />
+                                  <div className="flex flex-col gap-2 justify-start">
                                     <Typography
-                                      variant="small"
+                                      variant="h6"
+                                      color="white"
+                                      className="font-bold flex lg:flex-row flex-col gap-2"
+                                    >
+                                      {item.productId.name}
+                                      <Typography
+                                        variant="small"
+                                        color="white"
+                                        className="font-bold"
+                                      >
+                                        ({item.productId.category})
+                                      </Typography>
+                                    </Typography>
+                                    <Typography
+                                      variant="h6"
                                       color="white"
                                       className="font-bold"
                                     >
-                                      ({item.productId.category})
+                                      Price:${item.productId.price}
                                     </Typography>
-                                  </Typography>
-                                  <Typography
-                                    variant="h6"
-                                    color="white"
-                                    className="font-bold"
-                                  >
-                                    Price:${item.productId.price}
-                                  </Typography>
+                                    <button
+                                      className="text-teal-700 w-16 font-semibold"
+                                      onClick={() => {
+                                        removeProdCart(item.productId._id);
+                                      }}
+                                    >
+                                      Remove
+                                    </button>
+                                  </div>
+                                </div>
+                              </td>
+                              <td className="p-4">
+                                <div className="flex gap-4">
                                   <button
-                                    className="text-teal-700 w-16 font-semibold"
+                                    className="flex items-center justify-center"
                                     onClick={() => {
-                                      removeProdCart(item.productId._id);
+                                      handleDecreaseByOne(item.productId._id);
                                     }}
                                   >
-                                    Remove
+                                    <FaMinus
+                                      color="white"
+                                      className="cursor-pointer"
+                                      size={13}
+                                    />
+                                  </button>
+                                  <Typography color="white">
+                                    {item.quantity}
+                                  </Typography>
+                                  <button
+                                    className="flex items-center justify-center"
+                                    onClick={() => {
+                                      handleAddOneMore(item.productId._id);
+                                    }}
+                                  >
+                                    <IoAdd
+                                      color="white"
+                                      className="cursor-pointer "
+                                      size={20}
+                                    />
                                   </button>
                                 </div>
-                              </div>
-                            </td>
-                            <td className="p-4">
-                              <div className="flex gap-4">
-                                <button
-                                  className="flex items-center justify-center"
-                                  onClick={() => {
-                                    handleDecreaseByOne(item.productId._id);
-                                  }}
+                              </td>
+                              <td className="p-4 ">
+                                <Typography
+                                  variant="small"
+                                  color="white"
+                                  className="font-normal"
                                 >
-                                  <FaMinus
-                                    color="white"
-                                    className="cursor-pointer"
-                                    size={13}
-                                  />
-                                </button>
-                                <Typography color="white">
-                                  {item.quantity}
+                                  ${item.productId.price * item.quantity}
                                 </Typography>
-                                <button
-                                  className="flex items-center justify-center"
-                                  onClick={() => {
-                                    handleAddOneMore(item.productId._id);
-                                  }}
-                                >
-                                  <IoAdd
-                                    color="white"
-                                    className="cursor-pointer "
-                                    size={20}
-                                  />
-                                </button>
-                              </div>
-                            </td>
-                            <td className="p-4 ">
-                              <Typography
-                                variant="small"
-                                color="white"
-                                className="font-normal"
-                              >
-                                ${item.productId.price * item.quantity}
-                              </Typography>
-                            </td>
-                          </motion.tr>
-                        );
-                      }
-                    )}
-                  </tbody>
-                </table>
-              </CardBody>
-              <CardFooter
-                children
-                className="flex items-end justify-end text-white p-0"
-              >
-                <motion.div
-                  initial={{ opacity: 0, y: -50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 50 }}
-                  transition={{ duration: 1 }}
-                  className="border-t lg:w-[30%] w-full  flex flex-col  border-white"
+                              </td>
+                            </motion.tr>
+                          );
+                        }
+                      )}
+                    </tbody>
+                  </table>
+                </CardBody>
+                <CardFooter
+                  children
+                  className="flex items-end justify-end text-white p-0"
                 >
-                  <Card className="w-full bg-[#171720]">
-                    <CardBody className="flex flex-col gap-4">
-                      <div className="divide-y divide-gray-200">
-                        <div className="flex items-center justify-between last:pb-0">
-                          <div className="flex items-center gap-x-3">
-                            <div>
-                              <Typography color="white" variant="h6">
-                                Items
-                              </Typography>
+                  <motion.div
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 50 }}
+                    transition={{ duration: 1 }}
+                    className="border-t lg:w-[30%] w-full  flex flex-col  border-white"
+                  >
+                    <Card className="w-full bg-[#171720]">
+                      <CardBody className="flex flex-col gap-4">
+                        <div className="divide-y divide-gray-200">
+                          <div className="flex items-center justify-between last:pb-0">
+                            <div className="flex items-center gap-x-3">
+                              <div>
+                                <Typography color="white" variant="h6">
+                                  Items
+                                </Typography>
+                              </div>
                             </div>
+                            <Typography color="white" variant="h6">
+                              {TABLE_ROWS.length}
+                            </Typography>
                           </div>
-                          <Typography color="white" variant="h6">
-                            {TABLE_ROWS.length}
-                          </Typography>
                         </div>
-                      </div>
-                      <div className="divide-y divide-gray-200">
-                        <div className="flex items-center justify-between last:pb-0">
-                          <div className="flex items-center gap-x-3">
-                            <div>
-                              <Typography color="white" variant="h6">
-                                Total
-                              </Typography>
+                        <div className="divide-y divide-gray-200">
+                          <div className="flex items-center justify-between last:pb-0">
+                            <div className="flex items-center gap-x-3">
+                              <div>
+                                <Typography color="white" variant="h6">
+                                  Total
+                                </Typography>
+                              </div>
                             </div>
+                            <Typography color="white" variant="h6">
+                              ${totalCartValue}
+                            </Typography>
                           </div>
-                          <Typography color="white" variant="h6">
-                            ${totalCartValue}
-                          </Typography>
                         </div>
-                      </div>
 
-                      <Button
-                        size="sm"
-                        color="teal"
-                        className="flex justify-center items-center text-md font-light gap-2"
-                      >
-                        Proceed to Checkout
-                        <IoBagCheckOutline size={24} className="mb-2" />
-                      </Button>
-                    </CardBody>
-                  </Card>
-                </motion.div>
-              </CardFooter>
-            </Card>
+                        <Button
+                          size="sm"
+                          color="teal"
+                          className="flex justify-center items-center text-md font-light gap-2"
+                        >
+                          Proceed to Checkout
+                          <IoBagCheckOutline size={24} className="mb-2" />
+                        </Button>
+                      </CardBody>
+                    </Card>
+                  </motion.div>
+                </CardFooter>
+              </Card>
+            ) : (
+              ""
+            )}
           </div>
         )}
       </section>
